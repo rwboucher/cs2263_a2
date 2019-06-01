36f5ffec946046b6b86d8b2e97f543943aa0e3c4
Robert Boucher <philosophersvoice101@gmail.com>
2019-06-01 18:42:24 -0300

    Need to loop through lines.

--- sentiment1.c
+++ sentiment1.c
@@ -3,52 +3,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <stdbool.h>
-#define ARRAY_SIZE 60
+#define WORD_SIZE 20
 #define MAX_LINE_LENGTH 500
-
-
-void enterWord(char* retVal[])
-{
-  char searchWord[ARRAY_SIZE];
-  printf("Please enter the word to search for: ");
-  fgets(searchWord, ARRAY_SIZE, stdin);
-  *retVal = searchWord;
- }
 
 int main(int argc, char ** argv)
 {
     // Get the word to search for
-    char* getVal[ARRAY_SIZE] = {NULL};
-    enterWord(getVal);
-
+    char* getWord[WORD_SIZE];
+    printf("Please enter the word to search for: ");
+    fgets(*getWord, WORD_SIZE, stdin);
+    printf("Search word: %s", *getWord);
+    printf("\n");
+    
     // Open the file to be searched
     FILE* fileHandle;
     fileHandle = fopen(argv[1], "r");
     char oneLine[MAX_LINE_LENGTH];
 
-    // Search for the entered word
-    int reviewVal = 0;
-    char* reviewWord[MAX_LINE_LENGTH];
+    // Initialize variables
+    int reviewVal = 0, same = 99;
     char* tokenized[80];
-    //while(
+    int reviewTotal = 0, hits = 0, reviewAvg = 0;
+
+    //while((
     fgets(oneLine, sizeof(oneLine), fileHandle);
-    printf("%s", oneLine);
-    tokenized[0] = strtok(oneLine, " ");
-    tokenized[1] = strtok(NULL, " ");
-    printf("Got the token.\n");
-    printf("%s\n", tokenized[0]);
-    printf("%s\n", tokenized[1]);
-    // != NULL)
-    
+    printf("%s\n", oneLine);
+    //!= NULL)
+    //{
+        int i = 0;
+        tokenized[i] = strtok(oneLine, " ");
+        reviewVal = atoi(tokenized[i]);
         
+        while(tokenized[i] != NULL)
+        {
+            i++;
+            tokenized[i] = strtok(NULL, " ");
+            if(tokenized[i] != NULL)
+            {
+                same = strcasecmp(*getWord, tokenized[i]);
+                printf("getWord: %s\n", *getWord);
+                printf("same: %d\n", same);
+                printf("tokenized: %s\n\n", tokenized[i]);
+                if(same == 0)
+                {
+                    hits++;
+                    reviewTotal+=reviewVal;                    
+                }
+            }
+                    
+        } 
+    //}
     
-    
-
-
-
-    fclose(fileHandle);
-
-
-    
+    printf("%s appeared %d times\n", *getWord, hits);
+    fclose(fileHandle);   
 }
